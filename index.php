5915a724343870cac3546e1e349f50e2cb1c7f1e
HiMBixD <tuananvip123@gmail.com>
2019-10-15 10:13:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,10 +12,10 @@ $db = parse_url(getenv("DATABASE_URL"));
 						);
 		
 			$productid=$_GET['proid'];
-			$sqlx = "SELECT * from product where ProductId = $productid";
+			$sqlx = "SELECT * from product where ProductId = 10";
             $stmt1= $pdo->prepare($sqlx);
             $stmt1->setFetchMode(PDO::FETCH_ASSOC);
             $stmt1->execute();
             $producttable = pg_fetch_all($stmt1);
-            echo "zxc"+"<li> $producttable </li>";
+            echo "zxc"."<li> $producttable </li>";
  ?>
